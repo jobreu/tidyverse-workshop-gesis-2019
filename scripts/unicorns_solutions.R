d502221857166c1a7fdabd03cb476db065338de9
jobreu <jbreuer83@gmail.com>
2019-04-09 15:04:54 +0200

    added Unicorn data

---
+++ scripts/unicorns_solutions.R
@@ -0,0 +1,55 @@
+library(dplyr)
+library(readxl)
+
+# read in data ####
+observations <- read_excel('observations.xlsx')
+sales <- read_excel('sales.xlsx')
+
+glimpse(observations)
+glimpse(sales)
+
+# What is strange here?
+
+?read_excel
+
+# Fix this
+sales <- read_excel('sales.xlsx',  range = cell_cols("A:C")) %>% 
+    bind_cols(read_excel('sales.xlsx',  range = cell_cols("H")))
+
+sales
+observations
+
+# Prepare for merging ####
+observations %>% 
+    select(countryname) %>% 
+    distinct()
+
+sales %>% 
+    select(countryname) %>% 
+    distinct()
+
+sales <- sales %>% 
+    rename(countryname = name_of_country) %>% 
+    mutate(countryname = recode(countryname, "AUSTRIA" = "Austria",
+                                "FRANCE" = "France",
+                                "GERMANY" = "Germany",
+                                "NETHERLANDS" = "Netherlands",
+                                "SWITZERLAND" = "Switzerland"))
+
+sales
+observations
+
+# Filter cases ####
+# Let's say we are only interested in German-speaking countries
+observations_ger <- observations %>% 
+    filter(countryname != "France" & countryname != "Netherlands")
+# present/discuss other/alternative filter operations?
+
+observations_ger
+
+# Select variables ####
+total <- sales %>% 
+    select(-bikes)
+# present/discuss other/alternative variants of select?
+
+total
